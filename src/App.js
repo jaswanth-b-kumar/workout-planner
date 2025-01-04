d2dc87f6914970c685e383b48149dae39d71dfcd
jaswanth <jaswanth.k.bevara@gmail.com>
2025-01-04 01:58:50 +0000

    Added breakpoints and stylng for form

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import { Col, Container, Row } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-      <Container fluid>
-        <Row>
-          <Col>
+      <Container fluid="xxl" className='p-4 p-md-5 h-100'>
+        <Row className='align-items-stretch mx-xxl-5 p-md-3 h-100 homepage-container'>
+          <Col sm={12} lg={3} className='p-3'>
             <FormComponent />
           </Col>
-          <Col>
+          <Col sm={12} md={9} className='p-3'>
             <TableComponent />
           </Col>
         </Row>
