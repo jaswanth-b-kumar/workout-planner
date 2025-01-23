3b74e41e07b1f00016859158b76947b5d9dd7644
Jaswanth <jaswanth.k.bevara@gmail.com>
2025-01-23 23:12:59 +0000

    Redesigned the backgrounds and table

--- src/App.js
+++ src/App.js
@@ -13,28 +13,14 @@ function App() {
   return (
     <div className="App">
       <Container fluid="xxl" className='p-4 p-md-5 h-100'>
-        <Row className='align-items-stretch mx-xxl-5 p-md-3 h-100 homepage-container'>
-          <Col sm={12} lg={3} className='p-3'>
+        <Row className='align-items-stretch mx-xxl-5 h-100 homepage-container'>
+          <Col sm={12} lg={3} className='p-3 form-container'>
             <FormComponent />
           </Col>
-          <Col sm={12} md={9} className='p-3 h-100 d-flex flex-column align-items-center'>
+          <Col sm={12} md={9} className='p-0 h-100 d-flex flex-column align-items-center'>
             <TableComponent editModal={handleShow}/>
           </Col>
         </Row>
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Edit Workout</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
       </Container>
     </div>
   );
